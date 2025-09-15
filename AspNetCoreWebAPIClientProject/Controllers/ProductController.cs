72bd3b1737043a456da1b4974b188a4bc8374b02
gregoryb79 <gregoryb79@gmail.com>
2025-09-15 13:04:02 +0300

    client and server side works, code clean

---
+++ AspNetCoreWebAPIClientProject/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using AspNetCoreWebAPIClientProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace AspNetCoreWebAPIClientProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl;
+
+        public ProductController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _apiBaseUrl = configuration["ApiBaseUrl"];
+        }
+
+        // GET: ProductController1
+        public async Task<ActionResult> Index()
+        {
+            List<Product> products = new List<Product>();            
+
+            var responce = await _httpClient.GetAsync($"{_apiBaseUrl}/api/Products");                                  
+            string apiResponce = await responce.Content.ReadAsStringAsync();
+            products = JsonConvert.DeserializeObject<List<Product>>(apiResponce);       
+            
+            return View(products);
+        }
+
+        // GET: ProductController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
